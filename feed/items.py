1dd4988b5b9c196e9356381d6fff51f45eb30ddc
Aslan Varoqua <aslan.varoqua@gmail.com>
2018-01-11 01:08:22 -0500

    Update items.py

--- feed/items.py
+++ feed/items.py
@@ -7,8 +7,29 @@
 
 import scrapy
 
-
+# -*- This represents a JSON Object that will enter the database -*-
 class FeedItem(scrapy.Item):
+    # formats a category into normalized format
+    # @parameter cats: String
+    # @returns price: Float
+    def to_google_taxonomy(cats):
+        with open('./cats.csv', 'rb') as csvfile:
+            catreader = csv.reader(csvfile, delimiter=';')
+            
+            google_taxonomy_code = None
+            
+            for row in catreader:
+                if row[0].lower() in cats:
+                    google_taxonomy_code =  row[1]
+                    
+            return google_taxonomy_code
+                    
+    # formats price into normalized format
+    # @parameter price: String or Int 
+    # @returns price: Float
+    def parse_price(price):
+        return "{0:.2f}".format(float(price) / 100.00)
+                        
     product_id = scrapy.Field()
     store_id = scrapy.Field()
     brand = scrapy.Field()
@@ -22,7 +43,3 @@ class FeedItem(scrapy.Item):
     description = scrapy.Field()
     description_legacy = scrapy.Field()
     gender = scrapy.Field()
-
-
-   # product_id,store_id,brand,image_link,title,link,price,google_product_category,color,size,description,gender = scrapy.Field()
-
